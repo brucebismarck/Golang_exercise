022aedeb59b979289c12d22640cc0d3462499697
brucebismarck <brucebismarck@gmail.com>
2018-11-15 11:11:16 -0500

    updated colorful

--- ex3.5/mandelbrot.go
+++ ex3.5/mandelbrot.go
@@ -20,7 +20,7 @@ func main() {
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
-			//Image `point (px,py) represents complex value z.
+			//Image point (px,py) represents complex value z.
 			img.Set(px, py, mandelbrot(z))
 		}
 	}
@@ -29,13 +29,13 @@ func main() {
 
 func mandelbrot(z complex128) color.Color {
 	const iterations = 200
-	const contrast = 15
+	//const contrast = 15
 
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			return color.RGBA{255 - 15*n, 255 -  n, 255 - 8*n, 54}
 		}
 	}
 	return color.Black
