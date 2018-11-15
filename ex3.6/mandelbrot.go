70dc6c06005231c82b06fc0b97eb07b9f2b602a0
brucebismarck <brucebismarck@gmail.com>
2018-11-15 11:30:27 -0500

    commit

--- ex3.6/mandelbrot.go
+++ ex3.6/mandelbrot.go
@@ -13,18 +13,44 @@ func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 1024, 1024
+		epsX                   = (xmax - xmin) / width
+		epsY                   = (ymax - ymin) / height
 	)
+
+	offX := []float64{-epsX, epsX}
+	offY := []float64{-epsY, epsY}
+
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
+			// create subpixel
+			subPixels := make([]color.Color, 0) //length is 0
+			for i := 0; i < 2; i++ {            // split to four so 2*2
+				for j := 0; j < 2; j++ {
+					z := complex(x+offX[i], y+offY[j])
+					subPixels = append(subPixels, mandelbrot(z))
+				}
+			}
 			//Image point (px,py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, avg(subPixels))
 		}
 	}
 	png.Encode(os.Stdout, img) //Note: ignoring errors
+}
+
+func avg(colors []color.Color) color.Color {
+	var r, g, b, a uint16
+	n := len(colors)
+	for _, c := range colors {
+		r1, g1, b1, a1 := c.RGBA()
+		r += uint16(r1 / uint32(n))
+		g += uint16(g1 / uint32(n))
+		b += uint16(b1 / uint32(n))
+		a += uint16(a1 / uint32(n))
+	}
+	return color.RGBA64{r, g, b, a}
 }
 
 func mandelbrot(z complex128) color.Color {
