b4adae57a834d3ad1908f27da0aa3fd89a4c032e
brucebismarck <brucebismarck@gmail.com>
2018-11-17 13:38:34 -0500

    constant

---
+++ ex3.13/const.go
@@ -0,0 +1,10 @@
+// ex3.13 is a short definition of byte unit constants.
+package byteconst
+
+const (
+	// Write const declarations for KB, MB, up through YB as compactly as you can.
+	KB = 1024
+	MB = 1024 * KB
+	GB = 1024 * MB
+	PB = 1024 * GB
+)
