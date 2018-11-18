52bc26d14da6f24d2f00f00adcd26c0b6a1ef3b2
brucebismarck <brucebismarck@gmail.com>
2018-11-18 18:06:07 -0500

    rotate

--- ch4/ex4.3/reverseNew.go
+++ ch4/ex4.3/reverseNew.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 // NewReverse reverses a slice of ints in place
-func reverse(ints *[5]int) *[5]int {// restricted length
-	for i:=0; i < len(ints)/2; i++ {// because len(ints) is a integer, i < 2
+func reverse(ints *[5]int) *[5]int { // restricted length
+	for i := 0; i < len(ints)/2; i++ { // because len(ints) is a integer, i < 2
 		end := len(ints) - 1 - i
 		ints[i], ints[end] = ints[end], ints[i]
 	}
