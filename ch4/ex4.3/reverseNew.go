e9ae1539c7e6552466b41db7e785b193d9425091
brucebismarck <brucebismarck@gmail.com>
2018-11-18 15:10:50 -0500

    reverse with pointer

---
+++ ch4/ex4.3/reverseNew.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// NewReverse reverses a slice of ints in place
+func reverse(ints *[5]int) *[5]int {// restricted length
+	for i:=0; i < len(ints)/2; i++ {// because len(ints) is a integer, i < 2
+		end := len(ints) - 1 - i
+		ints[i], ints[end] = ints[end], ints[i]
+	}
+	return ints
+}
+
+func main() {
+	s := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(reverse(&s))
+}
