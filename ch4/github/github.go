32f8ca8276ec6982181b736431eec5efe1a0a3e8
brucebismarck <brucebismarck@gmail.com>
2018-11-25 11:34:26 -0500

    interesting

--- ch4/github/github.go
+++ ch4/github/github.go
@@ -7,12 +7,15 @@ import (
 	"net/url"
 	"strings"
 	"time"
+	"os"
+	"bytes"
 )
 
 // IssuesURL is the starting address for api get
 // Exported type should have comment or unexported
 // 暴露给别的包的东西必须给注释
 const IssuesURL = "https://api.github.com/search/issues"
+const APIURL = "https://api.github.com"
 
 // IssuesSearchResult is the return total count + issue
 type IssuesSearchResult struct {
@@ -59,4 +62,81 @@ func SearchIssues(terms []string) (*IssuesSearchResult, error) {
 	}
 	resp.Body.Close()
 	return &result, nil
+}
+
+//GetIssue is used to get single issue from Github issue tracker
+func GetIssue(owner string, repo string, number string) (*Issue, error) {
+	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues", number}, "/")
+	resp, err := get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close() //A defer statement defers the execution of a function until the surrounding function returns. 
+	// The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+
+	var issue Issue
+	if err := json.NewDecoder(resp.Body).Decode(&issue); err != nil {
+		return nil, err
+	}
+	return &issue, nil
+}
+
+func get(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("can't get %s: %s", url, resp.Status)
+	}
+	return resp, nil
+}
+
+// GetIssues I dont know why this
+func GetIssues(owner string, repo string) ([]Issue, error) {
+	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues"}, "/")
+	resp, err := get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var issues []Issue
+	if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
+		return nil, err
+	}
+	return issues, nil
+}
+
+// EditIssue enable user to edit issue
+func EditIssue(owner, repo, number string, fields map[string]string) (*Issue, error) {
+	buf := &bytes.Buffer{}
+	encoder := json.NewEncoder(buf)
+	err := encoder.Encode(fields)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues", number}, "/")
+	req, err := http.NewRequest("PATCH", url, buf)
+	req.SetBasicAuth(os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_PASS"))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to edit issue: %s", resp.Status)
+	}
+	var issue Issue
+	if err = json.NewDecoder(resp.Body).Decode(&issue); err != nil {
+		return nil, err
+	}
+	return &issue, nil
+
 }
