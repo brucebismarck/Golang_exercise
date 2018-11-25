f07d4e3c4019ec763b21af1b85b889555a34ff08
brucebismarck <brucebismarck@gmail.com>
2018-11-25 10:44:33 -0500

    Update ch4/ex4.10/issues.go

--- ch4/ex4.10/issues.go
+++ ch4/ex4.10/issues.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"go_exercises/Golang_exercise/ch4/github"
 )
@@ -14,9 +15,54 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%d issues:\n", result.TotalCount)
+	format := "#%-5d %9.9s %.55s\n"
+	now := time.Now()
+
+	pastDay := make([]*github.Issue, 0) // Not capitalized, internal using only
+	pastMonth := make([]*github.Issue, 0)
+	pastYear := make([]*github.Issue, 0)
+	evenOlder := make([]*github.Issue, 0)
+
+	day := now.AddDate(0, 0, -1) //AddDate(years int, months int, dates, int)
+	month := now.AddDate(0, -1, 0)
+	year := now.AddDate(-1, 0, 0)
+
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		switch {
+		case item.CreatedAt.After(day): // send to each categories
+			pastDay = append(pastDay, item)
+		case item.CreatedAt.After(month) && item.CreatedAt.Before(day):
+			pastMonth = append(pastMonth, item)
+		case item.CreatedAt.After(year) && item.CreatedAt.Before(month):
+			pastYear = append(pastYear, item)
+		default:
+			evenOlder = append(evenOlder, item)
+		}
+	}
+
+	if len(pastDay) > 0 {
+		fmt.Printf("\nPast day:\n")
+		for _, item := range pastDay {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+	if len(pastMonth) > 0 {
+		fmt.Printf("\nPast month:\n")
+		for _, item := range pastMonth {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+	if len(pastYear) > 0 {
+		fmt.Printf("\nPast year:\n")
+		for _, item := range pastYear {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+
+	if len(evenOlder) > 0 {
+		fmt.Printf("\nMore than one year:\n")
+		for _, item := range evenOlder {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
 	}
 }
